bec9756de34e73108ba1b4d0a5f734ef5be0f25d
Justin Zhao <justin-zhao@qq.com>
2015-10-12 15:18:45 +0800

    Improve board connect
    Signed-off-by: Justin Zhao <justin-zhao@qq.com>

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ PRJDIR="`dirname $0`"
 cd $PRJDIR
 
 # Install config files
-sudo mkdir -p $CFGPATH
+sudo mkdir -p $CFGPATH 2>/dev/null
 
 if [ ! -f "$CFGPATH/boards.cfg" ]; then
     sudo cp boards.cfg $CFGPATH/
@@ -26,11 +26,21 @@ fi
 
 # Install binaries
 sudo cp ap7921-control $BINPATH
+sudo cp board_check $BINPATH
 sudo cp board_connect $BINPATH
 sudo cp board_reboot $BINPATH
 sudo cp cp_rootfs.sh $BINPATH
 sudo cp gencfg.sh $BINPATH
 sudo cp newboard $BINPATH
 sudo cp newuser $BINPATH
+
+. $CFGPATH/boardlab.cfg
+
+mkdir -p $ftp_dir/backup 2>/dev/null
+cp Image_D02 $ftp_dir/backup/
+cp hip05-d02.dtb $ftp_dir/backup/
+cp zImage_D01 $ftp_dir/backup/
+cp hip04-d01.dtb $ftp_dir/backup/
+cp mini-rootfs.cpio.gz $ftp_dir/backup/
 
 echo "Boardlab installed successfully!"
