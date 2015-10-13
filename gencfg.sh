eec9eeb5e59944af02aa88a0fd9e77bed8805efb
Justin Zhao <justin-zhao@qq.com>
2015-10-13 15:43:20 +0800

    Improve inituser, gencfg.sh
    Signed-off-by: Justin Zhao <justin-zhao@qq.com>

--- gencfg.sh
+++ gencfg.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
+LABCFG="/etc/boardlab/boardlab.cfg"
+. $LABCFG
+
 USER=`whoami`
+SVRIP=`ifconfig -a|grep -A 2 eth|grep "inet addr"|grep -v 127.0.0.1|grep -v "Bcast:0.0.0.0"|awk '{print $2}'|tr -d "addr:"|sed -n "1p"`
+
 DEFCFG=~/grub.conf
-echo "set timeout=5
+
+echo "#Description: default grub config file
+#author: automated
+
+set timeout=5
 set default=minilinux
+
 menuentry \"minilinux\" --id minilinux {
-        set root=(tftp,192.168.1.107)
-        linux /$USER/Image rdinit=/init crashkernel=256M@32M console=ttyS0,115200 earlycon=uart8250,mmio32,0x80300000
-        initrd /$USER/hulk-hip05.cpio.gz
-        devicetree /$USER/hip05-d02.dtb
+    set root=(tftp,$SVRIP)
+    linux /$USER/$img_d02 rdinit=/init crashkernel=256M@32M console=ttyS0,115200 earlycon=uart8250,mmio32,0x80300000
+    initrd /$USER/$mini_rootfs
+    devicetree /$USER/$dtb_d02
 }
-menuentry \"ubuntu\" --id ubuntu {
-        set root=(tftp,192.168.1.107)
-        linux /$USER/Image rdinit=/init console=ttyS0,115200 earlycon=uart8250,mmio32,0x80300000 root=/dev/nfs rw nfsroot=192.168.1.107:/home/hisilicon/ftp/$USER/ubuntu ip=:::::eth0:dhcp::
-       devicetree /$USER/hip05-d02.dtb
+
+menuentry \"Ubuntu D02\" --id ubuntu_d02 {
+    set root=(tftp,$SVRIP)
+    linux /$USER/$img_d02 rdinit=/init console=ttyS0,115200 earlycon=uart8250,mmio32,0x80300000 root=/dev/nfs rw nfsroot=$SVRIP:$ftp_dir/$USER/ubuntu ip=dhcp
+    devicetree /$USER/$dtb_d02
 }
-menuentry \"opensuse\" --id opensuse {
-        set root=(tftp,192.168.1.107)
-        linux /$USER/Image rdinit=/init console=ttyS0,115200 earlycon=uart8250,mmio32,0x80300000 root=/dev/nfs rw nfsroot=192.168.1.107:/home/hisilicon/ftp/$USER/opensuse ip=:::::eth0:dhcp::
-       devicetree /$USER/hip05-d02.dtb
+
+menuentry \"Ubuntu D01\" --id ubuntu_d01 {
+    set root=(tftp,$SVRIP)
+    linux /$USER/$img_d01 rdinit=/init console=ttyS0,115200 earlycon=uart8250,mmio32,0x80300000 root=/dev/nfs rw nfsroot=$SVRIP:$ftp_dir/$USER/ubuntu ip=dhcp
+    devicetree /$USER/$dtb_d01
 }" >$DEFCFG
-
-
--- install.sh
+++ install.sh
@@ -10,37 +10,50 @@ PRJDIR="`dirname $0`"
 cd $PRJDIR
 
 # Install config files
-sudo mkdir -p $CFGPATH 2>/dev/null
+mkdir -p $CFGPATH 2>/dev/null
 
 if [ ! -f "$CFGPATH/boards.cfg" ]; then
-    sudo cp boards.cfg $CFGPATH/
+    cp boards.cfg $CFGPATH/
 fi
 
 if [ ! -f "$CFGPATH/users.cfg" ]; then
-    sudo cp users.cfg $CFGPATH/
+    cp users.cfg $CFGPATH/
 fi
 
 if [ ! -f "$CFGPATH/boardlab.cfg" ]; then
-    sudo cp boardlab.cfg $CFGPATH/
+    cp boardlab.cfg $CFGPATH/
 fi
 
 # Install binaries
-sudo cp ap7921-control $BINPATH
-sudo cp check_board $BINPATH
-sudo cp board_connect $BINPATH
-sudo cp board_reboot $BINPATH
-sudo cp cp_rootfs.sh $BINPATH
-sudo cp gencfg.sh $BINPATH
-sudo cp newboard $BINPATH
-sudo cp newuser $BINPATH
+cp ap7921-control $BINPATH
+if [ $? != 0 ]; then
+    echo "You should do this command with \"sudo\" before it"
+    exit
+fi
+cp check_board $BINPATH
+cp board_connect $BINPATH
+cp board_reboot $BINPATH
+cp cp_rootfs.sh $BINPATH
+cp gencfg.sh $BINPATH
+cp inituser $BINPATH
+cp newuser $BINPATH
 
 . $CFGPATH/boardlab.cfg
 
-mkdir -p $ftp_dir/backup 2>/dev/null
-sudo cp Image_D02 $ftp_dir/backup/
-sudo cp hip05-d02.dtb $ftp_dir/backup/
-sudo cp zImage_D01 $ftp_dir/backup/
-sudo cp hip04-d01.dtb $ftp_dir/backup/
-sudo cp mini-rootfs.cpio.gz $ftp_dir/backup/
+mkdir -p $ftp_dir 2>/dev/null
+chmod 777 $ftp_dir
+mkdir -p $ftp_dir/$ftp_bak 2>/dev/null
+cp $img_d01 $ftp_dir/$ftp_bak/
+cp $dtb_d01 $ftp_dir/$ftp_bak/
+cp $img_d02 $ftp_dir/$ftp_bak/
+cp $dtb_d02 $ftp_dir/$ftp_bak/
+cp $mini_rootfs $ftp_dir/$ftp_bak/
+
+grep "inituser" /etc/skel/.bashrc >/dev/null
+if [ $? = 1 ]; then
+    echo "if [ -f /usr/bin/inituser ]; then
+    $BINPATH/inituser    
+fi" >> /etc/skel/.bashrc
+fi
 
 echo "Boardlab installed successfully!"
